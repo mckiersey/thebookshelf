8e82a8f3edca94bc9a7286833e375fc531f4065c
Sean McKiernan <seanmckiernan01@gmail.com>
2020-06-07 16:51:28 +0100

    V1 pre-test FINAL

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,5 +1,5 @@
 // ROUTES FILE TO SET UP LINK TO DATABASE...
-const bcrypt = require('bcrypt');
+//const bcrypt = require('bcrypt');
 
 
 
@@ -15,21 +15,37 @@ const router = app => {
   
 // REDIRECTS
 app.get('/index', (request, response)=> {
-    console.log('sending to home...')
+    console.log('sending to index...')
     response.render('index')
 
 });
 
 app.get('/home', (request, response)=> {
     console.log('sending to home...')
-    response.render('home')
+    var unloggedUserName = request.query.unloggedUserName
+    console.log('unlogged user bookshelf', unloggedUserName)
+    let j =[];
+    //sample json
+    j.push({data:"hi"});
+
+    response.render('home',{json:j});
+    //response.render('home', {dataToPass: "passToFrontTest"})
 
 });
 
 
 
+
+
+
 app.get('/signUp', (request, response)=> {
-    console.log('sending to home...')
+    console.log('sending to sign up...')
+    response.render('signInSignUp')
+
+});
+
+app.get('/signOut', (request, response)=> {
+    console.log('Logging out & sending to back to sign up...')
     response.render('signInSignUp')
 
 });
@@ -42,7 +58,7 @@ app.get('/signUp', (request, response)=> {
         var NewuserName = request.body.userName
         var Newpassword = request.body.password
 
-        console.log('encrypting credentials...')
+        console.log('encrypting credentials...')    
         //To add later: password encryption
         //const salt = await bcrypt.genSalt(10)
         //const hashedPassword = await bcrypt.hash(Newpassword, salt)
@@ -78,11 +94,7 @@ app.post('/auth', function (request, response) {
         [username, password], function (error, results, fields) {
                 if (results.length > 0) {
                     console.log('Credentials match..')
-                    
-                    //response.redirect('/home')
                     response.sendStatus(200);
-
-                    //console.log('redirecting...')
                 } else {
                     response.send('Incorrect Username and/or Password!');
                     console.log('credentials do not match')
@@ -100,57 +112,112 @@ app.post('/auth', function (request, response) {
 
 
 
+/////////////////////////////////////////////
+///////////////////////////////// GET DATA 
+/////////////////////////////////////////////
 
-    //FETCH DATA
+
+    // FETCH USERS;
+    app.get('/users', (request, response) => {
+        pool.query(`SELECT DISTINCT user FROM contentData`, (error, result) => {
+            if(error) throw error;
+            
+            response.send(result);
+            console.log('Here is the list of all bookshelf users! What wonderful people', result)
+        });
+    });
+
+
+   // FETCH PLAYLISTS;
+   app.get('/playlists', (request, response) => {
+    userName = request.query.userName
+    console.log('playlists user name is:' , userName)
+    pool.query(`SELECT LINK, context FROM contentData WHERE user = "${userName}" AND type = "music"`, (error, result) => { 
+        if (error) throw error;
+        response.send(result);
+        console.log('playlist results', result)
+    });
+});
+
     // Fetch videoLinks;
-
     app.get('/videos', (request, response) => {
+            console.log('video user name', userName)
         pool.query(`SELECT link FROM contentData WHERE user = '${userName}' AND type = 'video'`, (error, result) => {
             if (error) throw error;
             console.log('Video links requested...')
             response.send(result);
         });
     });
-    //FETCH ARTICLES
 
+    // FETCH PODCASTS
+    app.get('/podcasts', (request, response)=>{
+        userName = request.query.userName
+        context = request.query.podContext
+        console.log('the user is',userName, 'and the context is',context)
+        pool.query(`SELECT DISTINCT link, caption FROM contentData WHERE user = '${userName}' AND type = 'podcast' AND context = "${context}"`, (error, result) => {
+            if (error) throw error;
+            console.log('Fetching podcast type:', context, 'for user', userName)
+            response.send(result);
+        });
+    });
+
+    //FETCH ARTICLES
     app.get('/articles', (request, response) => {
-        pool.query(`SELECT link FROM contentData WHERE user = '${userName}' AND type = 'article'`, (error, result) => {
+        pool.query(`SELECT DISTINCT link, caption FROM contentData WHERE user = '${userName}' AND type = 'article'`, (error, result) => {
             if (error) throw error;
             console.log('Article links requested...')
             response.send(result);
-
         });
     });
 
     // FETCH CAROUSEL IMAGES;
     app.get('/carouselImages', (request, response) => {
         userName = request.query.userName
-
         console.log('Carousel user name is:' , userName)
         pool.query(`SELECT LINK FROM contentData WHERE user = "${userName}" AND type = "image" AND context = "carouselImage"`, (error, result) => {
             if (error) throw error;
             response.send(result);
-
         });
     });
 
     //FETCH GALLERY IMAGES:
     app.get('/images', (request, response) => {
-        console.log('Gallery user name is:' , userName)
-        pool.query(`SELECT LINK, CAPTION FROM contentData WHERE user = "${userName}" AND type = "image" AND context = "gridImage"`, (error, result) => {
+        pool.query(`SELECT DISTINCT LINK, CAPTION FROM contentData WHERE user = "${userName}" AND type = "image" AND context = "gridImage"`, (error, result) => {
             if (error) throw error;
             response.send(result);
         });
+    });
 
+      //FETCH PROFILE PICTURE:
+      app.get('/profilePictures', (request, response) => {
+        pool.query(`SELECT DISTINCT LINK FROM contentData WHERE user = "${userName}" AND type = "image" AND context = "profilePicture"`, (error, result) => {
+            if (error) throw error;
+            response.send(result);
+        });
     });
 
 
-    //POST NEW DATA
+/////////////////////////////////////////////
+///////////////////////////////// POST DATA 
+/////////////////////////////////////////////
 
- 
+//POST MUSIC PLAYLIST
+    app.post('/playlists', (request, response) => {
+        console.log('playlist post being sent');
+        var userName = request.body.user;
+        var playlistID = request.body.musicInput;
+        var playlistPlatform = request.body.musicType;
+
+        console.log('username:', userName, 'playlist is', playlistID, 'platform is', playlistPlatform)
+        pool.query(`INSERT INTO contentData (user, link, type, context) VALUES("${userName}", '${playlistID}', 'music','${playlistPlatform}');`, (error, result) => {
+            if (error) throw error;
+            console.log('error type:', error);
+            response.status(201).send(`Playlist sucessfully added. Refesh the page to see it!`);
+            console.log('Post Success!');
+        });
+    });
 
     //POST VIDEO LINK
-
     app.post('/videos', (request, response) => {
         console.log('post sent');
         var userName = request.body.user;
@@ -158,25 +225,57 @@ app.post('/auth', function (request, response) {
         var videoLink = request.body.videoInput;
         console.log('username:', userName)
         console.log('data is', videoLink)
-        pool.query(`INSERT INTO contentData (user, link, type, context) VALUES("${userName}", '${videoLink}', 'video', '');`, (error, result) => {
+        pool.query(`INSERT INTO contentData (user, link, type) VALUES("${userName}", '${videoLink}', 'video');`, (error, result) => {
             if (error) throw error;
             console.log('error type:', error);
-            response.status(201).send(`Video sucessfully added...`);
+            response.status(201).send(`Video sucessfully added. Refesh the page to see it!`);
             console.log('Post Success!');
         });
     });
 
+    // POST PODCAST
+    app.post('/podcasts', (request, response)=>{
+        console.log('Adding podcast to database')
+        console.log(request.body)
+        podcastEpDescription = request.body.caption
+        podcastLink = request.body.link
+        userName = request.body.user
+        podcastContext = request.body.context
+        selectedPodcast = request.body.podcastName
+        podEpCaption = request.body.caption
+         if(podcastContext==='episode'){
+             var podcastCaption = podEpCaption
+         } else{
+             var podcastCaption = selectedPodcast
+         }
+
+        console.log('selected podcast', selectedPodcast)
+        console.log('Description podcast', podcastEpDescription)
+        console.log('Link podcast', podcastLink)
+        console.log('Username podcast', userName)
+        console.log('context podcast', podcastContext)
+        console.log('caption podcast', podcastCaption)
+
+        pool.query(`INSERT INTO contentData (user, link, type, context, caption) VALUES("${userName}", "${podcastLink}", 'podcast', "${podcastContext}", "${podcastCaption}");`, (error, result) => {
+            if (error) throw error;
+            console.log('error type:', error);
+            response.status(201).send(`Podcast added to your shelf...`);
+            console.log('Podcast Post Success!');
+        });
+        
+    });
     // POST ARTICLE LINK
     app.post('/articles', (request, response) => {
         console.log('article being posted...');
         console.log(request.body)
         var userName = request.body.accountOwner
         var articleLink = request.body.articleInput
-        console.log('Inserting article:', articleLink, ' for user: ', userName)
-        pool.query(`INSERT INTO contentData (user, link, type, context) VALUES("${userName}", '${articleLink}', 'article', '');`, (error, result) => {
+        var articleCaption = request.body.articleDescrpition
+        console.log('Inserting article:', articleLink, ' for user: ', userName, 'with caption', articleCaption)
+        pool.query(`INSERT INTO contentData (user, link, type, caption) VALUES("${userName}", '${articleLink}', 'article', "${articleCaption}");`, (error, result) => {
             if (error) throw error;
             console.log('error type:', error);
-            response.status(201).send(`Article sucessfully added...`);
+            response.status(201).send(`Article sucessfully added to your shelf. Refresh the page to see it!`);
             console.log('Post Success!');
         });
 
@@ -210,22 +309,39 @@ app.post('/auth', function (request, response) {
 
     });
 
-    //DELETE DATA
+/////////////////////////////////////////////
+///////////////////////////////// DELETE DATA 
+/////////////////////////////////////////////
 
     // Delete a videoLink;
     app.delete('/videos', (request, response) => {
         var videoToDelete = request.query.deleteMeVideo
         console.log('request to delete this data:', videoToDelete, 'for user:', userName);
-        console.log('deleting video...')
         pool.query(`DELETE FROM contentData WHERE user = '${userName}' AND type = 'video' AND link = '${videoToDelete}'`, (error, result) => {
             if (error) throw error;
             console.log(response.result);
 
-            response.status(201).send(`Video deleted`);
+            response.status(201).send(`Video removed from your shelf. Refresh the page to see the changes!`);
 
-            console.log('video successfully deleted')
+            console.log('Video successfully deleted. ')
         });
     });
+
+     // Delete a podcast;
+     app.delete('/podcasts', (request, response) => {
+         console.log('podcast delete request', request.query)
+        var selectedPodcast = request.query.deleteMePodcast
+        var userName = request.query.deleteMeUserName
+        var podcastContext = request.query.podcastContext
+        console.log('request to delete this podcast:', selectedPodcast, 'for user:', userName, 'with context', podcastContext);
+        pool.query(`DELETE FROM contentData WHERE user = '${userName}' AND type = 'podcast' AND context = '${podcastContext}' AND link = '${selectedPodcast}'`, (error, result) => {
+            if (error) throw error;
+             response.status(201).send(`Podcast removed...`);
+            console.log('Podcast successfully successfully removed from shelf')
+        });
+    });
+
+
 
     // Delete a Carousel image;
     app.delete('/carouselImages', (request, response) => {
@@ -263,19 +379,17 @@ app.post('/auth', function (request, response) {
     });
 
         // Delete an articleLink;
-
         app.delete('/articles', (request, response) => {
-            var articleToDelete = request.query.deleteMeArticle
+            var articleToDelete = request.query.deleteMeArticleCaption
             var userName = request.query.deleteMeUserName
 
             console.log('request to delete this data:', articleToDelete, 'for user:', userName);
             console.log('deleting article...')
-            pool.query(`DELETE FROM contentData WHERE name = '${userName}' AND type = 'article' AND link = '${articleToDelete}' AND type = 'article'`,
+            pool.query(`DELETE FROM contentData WHERE user = '${userName}' AND type = 'article' AND caption = "${articleToDelete}"`,
                 (error, result) => {
                 if (error) throw error;
-                console.log(response.result);
 
-                response.status(201).send(`Article deleted`);
+                response.status(201).send(`Article successfully removed from your shelf. Refresh the page to see the changes.`);
                 console.log('the result is', result)
                 console.log('Article successfully deleted')
             });
