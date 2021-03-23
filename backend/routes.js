043492aa40c56d7301d1b7d84948b5f255138591
Sean McKiernan <seanmckiernan01@gmail.com>
2021-03-23 07:58:48 +0000

    First step to building a backend

--- backend/routes.js
+++ backend/routes.js
@@ -1,7 +1,12 @@
+// Podcast API Credentials
+const podcastCredentials = require("/ConfigPodcasts.js")
+const unirest = require('unirest');
 
 
 // DEFINE APP
 const router = app => {
+
+
 
     app.get('/', (request, response) => {
         LandingPage = "/SneakPeek/SneakPeek-Landing.html"
@@ -15,6 +20,22 @@ const router = app => {
     });
 
 
+    // PODCAST API
+
+
+    app.get('/api/podcasts', (req, res) => {
+        unirest.get(podcastAPIURL).header('X-ListenAPI-Key', podcastAPIKey).end((response) => {
+            //make sure response should be a JSON object
+            var thumbnail = response.body.results[0].thumbnail
+            var title = response.body.results[0].title_original
+            var url = response.body.results[0].listennotes_url
+            var id = response.body.results[0].id
+            var allText = response.body.results[0]
+
+            res.status(200).send([title, thumbnail, url, id, allText])
+
+        });
+    });
 
 };
 module.exports = router;
