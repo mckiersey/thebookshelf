107d178436bd95f3b903cd9a27ac5c942c64b4dd
Sean McKiernan <seanmckiernan01@gmail.com>
2021-03-23 08:25:43 +0000

    change branch

--- backend/routes.js
+++ backend/routes.js
@@ -1,5 +1,5 @@
 // Podcast API Credentials
-const podcastCredentials = require("/ConfigPodcasts.js")
+const podcastCredentials = require("./ConfigPodcasts.js")
 const unirest = require('unirest');
 
 
@@ -22,19 +22,23 @@ const router = app => {
 
     // PODCAST API
 
+    const TestPodcastURL = 'https://listen-api.listennotes.com/api/v2/search?q=star%20wars&sort_by_date=0&type=episode&offset=0&len_min=10&len_max=30&genre_ids=68%2C82&published_before=1580172454000&published_after=0&only_in=title%2Cdescription&language=English&safe_mode=0'
 
-    app.get('/api/podcasts', (req, res) => {
-        unirest.get(podcastAPIURL).header('X-ListenAPI-Key', podcastAPIKey).end((response) => {
-            //make sure response should be a JSON object
-            var thumbnail = response.body.results[0].thumbnail
-            var title = response.body.results[0].title_original
-            var url = response.body.results[0].listennotes_url
-            var id = response.body.results[0].id
-            var allText = response.body.results[0]
 
-            res.status(200).send([title, thumbnail, url, id, allText])
+    app.get('/api/podcasts', async (req, res) => {
+        const response = await unirest.get(TestPodcastURL).header('X-ListenAPI-Key', podcastCredentials.podcastAPIKey)
+        console.log(response.toJSON())
+        //make sure response should be a JSON object
+        var thumbnail = response.body.results[0].thumbnail
+        var title = response.body.results[0].title_original
+        var url = response.body.results[0].listennotes_url
+        var id = response.body.results[0].id
+        var allText = response.body.results[0]
+        console.log('PODCAST RESPONSE: ', thumbnail, title, url, id, allText)
 
-        });
+        res.status(200).send([title, thumbnail, url, id, allText])
+
+
     });
 
 };
