81e55cb6508d063c282bce7bdb0923b13cfb62ef
Sean McKiernan <seanmckiernan01@gmail.com>
2021-04-08 08:20:46 +0100

    pushing podcast work

--- backend/routes.js
+++ backend/routes.js
@@ -22,29 +22,36 @@ const router = app => {
 
 
     // PODCAST API
-
-    // const TestPodcastURL = 'https://listen-api.listennotes.com/api/v2/search?q=star%20wars&sort_by_date=0&type=episode&offset=0&len_min=10&len_max=30&genre_ids=68%2C82&published_before=1580172454000&published_after=0&only_in=title%2Cdescription&language=English&safe_mode=0'
-    //const podcastAPIKey = 'b46ef8cf6c8c4bae9fbfab6779be26c1'
-
-    app.get('/api/podcasts', async (req, res) => {
-        console.log(req.query.search)
-
-        var PodcastSearchTerm = req.query.search
+    app.post('/api/podcasts', async (req, res) => {
+        var PodcastSearchTerm = req.body.PodcastSearchTerm
         var PodcastSearchTermAPI = 'https://listen-api.listennotes.com/api/v2/search?q=' + PodcastSearchTerm + '&type=podcast'
         const response = await unirest.get(PodcastSearchTermAPI).header('X-ListenAPI-Key', sourceFile.podcastAPIKey)
-        //make sure response should be a JSON object
+
+        console.log(response.body.results[0])
+        var thumbnail = response.body.results[0].thumbnail
+        var title = response.body.results[0].title_original
+        var url = response.body.results[0].listennotes_url
+        var id = response.body.results[0].id
+        var allText = response.body.results[0]
+        res.status(200).send([title, thumbnail, url, id, allText])
+    });
+
+    // PODCAST EPISODE API
+    app.post('/api/podcasts/episodes', async (req, res) => {
+        console.log('episode api')
+        var PodcastSearchTerm = req.body.PodcastSearchTerm
+        var PodcastSearchTermAPI = 'https://listen-api.listennotes.com/api/v2/search?q=' + PodcastSearchTerm + '&type=episode'
+        const response = await unirest.get(PodcastSearchTermAPI).header('X-ListenAPI-Key', sourceFile.podcastAPIKey)
 
         var thumbnail = response.body.results[0].thumbnail
         var title = response.body.results[0].title_original
         var url = response.body.results[0].listennotes_url
         var id = response.body.results[0].id
         var allText = response.body.results[0]
-
         res.status(200).send([title, thumbnail, url, id, allText])
-
-
-
     });
+
+
 
 };
 module.exports = router;
